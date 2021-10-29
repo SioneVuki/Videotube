cc7d89e4df4e032addbcc41cb66197576aa9f0bb
HGP48 <hgp48@Thes-MacBook-Air-6.local>
2021-10-28 17:41:22 -0700

    First commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hoopology</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="assets/js/commonActions.js"></script>
+</head>
+<body>
+   
+    <div id="pageContainer">
+
+        <div id="mastHeadContainer">
+            <button class="navShowHide">
+                <img src="assets/images/icons/icons8-menu-36.png" alt="menu">
+            </button>
+        </div>
+
+
+        <div id="sideNavContainer" style="display:none;">
+
+        </div>
+
+        <div id="mainSectionContainer">
+
+            <div id="mainContentContainer"> 
+                ok
+            </div>
+
+        </div>
+
+    </div>
+
+</body>
+</html>
